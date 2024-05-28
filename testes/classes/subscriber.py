bf685c2c1ef84d1fc083b054b658bdd144e8fea9
Arthur Coelho Estevão <56831082+arthurcoelho442@users.noreply.github.com>
2024-05-28 14:06:19 -0300

    :construction: Ajustes

--- testes/classes/subscriber.py
+++ testes/classes/subscriber.py
@@ -1,17 +1,24 @@
 import logging
 import subprocess
+import random
+import os
 from time import sleep
 
 logging.basicConfig(level="INFO")
 
 def sub(num, path, node, port, topic, simTime):
-    cmd = f"mosquitto_sub -h {node} -p {port} -t {topic}"
-    logging.info(f"USER {num:02d} - {cmd}")
-    
-    arquivo = open(path + f'/sub_{num}.log', 'w')
+    path += "/subscribers"
+    try:
+        os.mkdir(path)
+    except: pass
+    path += f"/sub_{num}.log"
+    arquivo = open(path, 'w')
     arquivo.writelines(["Iniciado\n"])
     arquivo.close()
-    arquivo = open(path + f'/sub_{num}.log', 'a')
+    arquivo = open(path, 'a')
+    
+    cmd = f"mosquitto_sub -h {node} -p {port} -t {topic}"
+    logging.info(f"Subscriber {num:02d} - {cmd}")
     
     sub_process = subprocess.Popen(cmd.split(), stdout=arquivo,)
     
@@ -22,5 +29,34 @@ def sub(num, path, node, port, topic, simTime):
     sub_process.terminate()
     sub_process.wait()
     
+    arquivo.writelines(["Terminado"])
+    arquivo.close() 
+    
+def pub(num, path, node, port, topic, attribute, intermsg, stop_event):
+    path += "/publishers"
+    try:
+        os.mkdir(path)
+    except: pass
+    path += f"/pub_{num}.log"
+        
+    arquivo = open(path, 'w')
+    arquivo.writelines(["Iniciado\n"])
+    arquivo.close()
+    arquivo = open(path, 'a')
+    
+    i = 25
+    attr = '{"%s":' % attribute
+    character = '}'
+    while not stop_event.is_set():
+        data = random.randint(i-5, i+5)
+        i = data
+        cmd = f"mosquitto_pub -h {node} -p {port} -t {topic} -m \'{attr}{data}{character}\'"
+        logging.info(f"Publisher {num:02d} - {cmd}")
+        
+        sub_process = subprocess.Popen(cmd.split(), stdout=arquivo,)
+        sub_process.terminate()
+        sub_process.wait()
+        sleep(intermsg)
+        
     arquivo.writelines(["Terminado"])
     arquivo.close() 
--- testes/payload.py
+++ testes/payload.py
@@ -1,31 +1,55 @@
 import os
 import threading
+from time import sleep
 
 from dotenv import load_dotenv
 from classes.subscriber import sub
+from classes.subscriber import pub
 
 path = f'./{os.path.basename(__file__)[:-3]}/'
 load_dotenv(dotenv_path=path+".env")
 
 numSubs     = int(os.getenv('numSubscriber', 1))
+numPubs     = int(os.getenv('numPublishers', 1))
+simTime     = int(os.getenv('simTime', 0))
+
+host        = os.getenv('host', 'localhost')
+port        = int(os.getenv('port', 1883))
+topic       = os.getenv('topic', 'teste')
+attr        = os.getenv('attr', "value")
+
+msgTime     = int(os.getenv('msgTime', 1))
 result_path = os.getenv('result_path', "./")
 
-node    = os.getenv('host', 'localhost')
-port    = int(os.getenv('port', 1883))
-topic   = os.getenv('topic', 'teste')
-simTime = int(os.getenv('simTime', 0))
-
-multThread  = []
+multThread, stop_events  = [], []
 
 try:
     os.mkdir(result_path)
 except: pass
 
-for i in range(numSubs):
-    thread = threading.Thread(target=sub, args=(i+1, result_path, node, port, topic, simTime, ))
+# for i in range(numSubs):
+#     thread = threading.Thread(target=sub, args=(i+1, result_path, host, port, topic, simTime, ))
+#     multThread.append(thread)
+#     thread.start()
+    
+# # Verifica se todas as threads acabaram 
+# for thread in multThread:
+#     thread.join()
+
+for i in range(numPubs):
+    stop_event = threading.Event()
+    stop_events.append(stop_event)
+    thread = threading.Thread(target=pub, args=(i+1, result_path, host, port, topic, attr, msgTime, stop_event, ))
     multThread.append(thread)
     thread.start()
-    
+
+# Espera o tempo de execução finalizar
+sleep(simTime)
+
+# Sinaliza todas as threads para parar
+for event in stop_events:
+    event.set()
+
 # Verifica se todas as threads acabaram 
 for thread in multThread:
     thread.join()
