f001307451562a84b89da6324d01585ac3e470d2
Arthur Coelho Estevão <56831082+arthurcoelho442@users.noreply.github.com>
2024-05-31 11:30:56 -0300

    :construction: Ajuste MQTT

--- testes/classes/mqtt.py
+++ testes/classes/mqtt.py
@@ -4,35 +4,41 @@ import logging
 import random
 import os
 from time import sleep
+from dotenv import load_dotenv
 from paho.mqtt.client import Client
+import json
 
 logging.basicConfig(level="INFO")
 
 class MQTT():
     # Descritor
-    def __init__(self, host, port, topic, attribute) -> None:
-        self.__host           = host
-        self.__port           = port
-        self.__topic          = topic
-        self.__attribute      = attribute
+    def __init__(self, path) -> None:        
+        load_dotenv(dotenv_path=path+"/.env")
+
+        # config conection
+        self.__host             = os.getenv('host', 'localhost')
+        self.__attribute        = os.getenv('attr', "value")
+        self.__topic            = os.getenv('topic', 'teste')
+        self.__port             = int(os.getenv('port', 1883))
         
-        self.__stdout_arquivo = False
+        # config exec
+        self.__numSubs          = int(os.getenv('numSubscriber', 1))
+        self.__numPubs          = int(os.getenv('numPublishers', 1))
+        self.__simTime          = int(os.getenv('simTime', 10))
+        self.__msgTime          = int(os.getenv('msgTime', 100))
+        
+        # config path result
+        self.__path             = os.getenv('result_path', "./")
+        self.__stdout_arquivo   = eval(os.getenv('stdout_arquivo', 'False'))
+        
+        if self.__stdout_arquivo:
+            os.makedirs(self.__path, exist_ok=True)
+            
         self.__func           = self.__random_num
-    
+        
     # SET
-    def set_path(self, result_path):
-        os.makedirs(result_path, exist_ok=True)
-        self.__path    = result_path
-        self.__stdout_arquivo = True
-          
     def set_func(self, func):
         self.__func       = func
-    
-    def set_run(self, numSubs=1, numPubs=1, msgTime=100, simTime=10):
-        self.__numSubs  = numSubs
-        self.__numPubs  = numPubs
-        self.__msgTime  = msgTime
-        self.__simTime  = simTime
     
     # Private function
     def __random_num(self, attribute='teste', i=25):
@@ -40,10 +46,9 @@ class MQTT():
         i   = dic[attribute]
         return dic, i
     
-    def __prepare_log_file(self, id, path, role):
-        path += role
-        os.makedirs(path+role, exist_ok=True)
-        log_file_path = f"{path+role}{role[:4]}_{id}.log"
+    def __prepare_log_file(self, id, role):
+        os.makedirs(self.__path+role, exist_ok=True)
+        log_file_path = f"{self.__path+role}{role[:4]}_{id}.log"
         
         with open(log_file_path, 'w') as arquivo:
             arquivo.write("Iniciado\n")
@@ -51,55 +56,71 @@ class MQTT():
     
     # Public function   
     def sub(self, id, stop_event):
+        def on_message(client, userdata, message):
+            try:
+                mensagem = message.payload.decode('utf-8')
+                if(self.__stdout_arquivo and '{' in mensagem and '}' in mensagem):
+                    with open(f"{self.__path}/subscribers/sub_{id}.log", 'a') as arquivo:
+                        arquivo.write(f"{mensagem}\n")
+            except: pass
+            
+        def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                if(self.__stdout_arquivo):
+                    arquivo = self.__prepare_log_file(id, "/subscribers")
+                    arquivo.close()
+                client.subscribe(self.__topic)
+            else:
+                print(f"Falha na conexão, código de erro {rc}")
+        
         cmd = f"mosquitto_sub -h {self.__host} -p {self.__port} -t {self.__topic}"
         logging.info(f"Subscriber {id:02d} - {cmd}")
         
-        if(self.__stdout_arquivo):
-            arquivo = self.__prepare_log_file (id, self.__path, "/subscribers")
-            sub_process = subprocess.Popen(cmd.split(), stdout=arquivo,)
-        else:
-            sub_process = subprocess.Popen(cmd.split(), stdout=subprocess.DEVNULL,)
+        client = Client()
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.connect(self.__host, self.__port)
         
-        # Espera sinalização do evento
+        # Inicia o loop para processar mensagens
+        client.loop_start()
+
+        # Espera sinalização do evento de parada
         stop_event.wait()
-        
-        # Terminate the subprocess when the stop event is set
-        sub_process.terminate()
-        sub_process.wait()
-        
-        if(self.__stdout_arquivo):
-            arquivo.writelines(["Terminado"])
-            arquivo.close() 
+
+        # Desconexão do broker MQTT
+        client.loop_stop()
+        client.disconnect()
 
     def pub(self, id, stop_event):
-        if(self.__stdout_arquivo):
-            arquivo = self.__prepare_log_file (id, self.__path, "/publishers")
-            log = []
-        
+        def loop(arquivo = None):
+            _, i = self.__func(self.__attribute)
+            try:
+                while not stop_event.is_set():
+                    dic, i = self.__func(self.__attribute, i)
+                    
+                    os.system(f"{cmd} -m \'{dic}\'")
+                    client.publish(self.__topic, dic[self.__attribute])
+                    
+                    if(arquivo):
+                        arquivo.write(f"{dic}\n")
+                        
+                    sleep(self.__msgTime/1000)
+            except Exception as e:
+                print(e)
+                              
         cmd = f"mosquitto_pub -h {self.__host} -p {self.__port} -t {self.__topic}"
         logging.info(f"Publisher {id:02d} - {cmd}")
         
         client = Client()
         client.connect(self.__host, self.__port)
-        _, i = self.__func(self.__attribute)
-        try:
-            while not stop_event.is_set():
-                dic, i = self.__func(self.__attribute, i)
-                
-                os.system(f"{cmd} -m \'{dic}\'")
-                client.publish(self.__topic, dic[self.__attribute])
-                
-                if(self.__stdout_arquivo):
-                    log.append(f"{dic}\n")
-                    
-                sleep(self.__msgTime/1000)
-        except Exception as e:
-            print(e)
-        
         if(self.__stdout_arquivo):
-            log.append("Terminado")
-            arquivo.writelines(log)
+            arquivo = self.__prepare_log_file (id, "/publishers")
+            loop(arquivo)
+            arquivo.write("Terminado")
             arquivo.close()
+        else:
+            loop()
+        client.disconnect()
     
     def run(self):
         multThread, stop_events  = [], []
--- testes/payload.py
+++ testes/payload.py
@@ -1,21 +1,5 @@
-import os
-from dotenv import load_dotenv
 from classes.mqtt import MQTT
-
-path = f'./{os.path.basename(__file__)[:-3]}/'
-load_dotenv(dotenv_path=path+".env")
-
-host            = os.getenv('host', 'localhost')
-attr            = os.getenv('attr', "value")
-topic           = os.getenv('topic', 'teste')
-port            = int(os.getenv('port', 1883))
-numSubs         = int(os.getenv('numSubscriber', 1))
-numPubs         = int(os.getenv('numPublishers', 1))
-simTime         = int(os.getenv('simTime', 0))
-msgTime         = int(os.getenv('msgTime', 1))
-result_path     = os.getenv('result_path', "./")
-stdout_arquivo  = eval(os.getenv('stdout_arquivo', 'False'))
-
+import os
         
 def increasing_payload(attribute='teste', i=''):
     i   += 'x'
@@ -23,10 +7,6 @@ def increasing_payload(attribute='teste', i=''):
     return dic, i
   
 if __name__ == '__main__':
-    mqtt = MQTT(host, port, topic, attr)
-    if(stdout_arquivo):
-        mqtt.set_path(result_path)    
+    mqtt = MQTT(f'./{os.path.basename(__file__)[:-3]}')
     mqtt.set_func(increasing_payload)
-    mqtt.set_run(numSubs, numPubs, msgTime, simTime)
-    
     mqtt.run()
